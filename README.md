e3f028e00b9bd3a40b99299848565f5d3cfc31b5
ntthanhvy <nguyenthithanhvyjps15@gmail.com>
2022-07-03 23:16:22 +0700

    init readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tw-table-react
 table component with tailwind
+
+
+This is use to implement the usage of colSpan and rowSpan in html table
+using react dynamic rendering and tailwind css.
+
+Basic usage: is on App.js file.
---
+++ src/data.ts
@@ -0,0 +1,114 @@
+export default [
+	{
+		productId: 1,
+		productName: "Product 0",
+		productImageCode: "220622-853f8e3b-4f33-44d3-b0b4-ff1c98450498.jpg",
+		unit: "Unit 0",
+		startValue: {
+			amount: 1.0,
+			value: 1.0,
+		},
+		endValue: {
+			realValue: {
+				amount: 1.0,
+				value: 1.0,
+			},
+			calculateValue: {
+				amount: 1.0,
+				value: 1.0,
+			},
+		},
+		importValue: {
+			amount: 5.0,
+			value: 5.0,
+		},
+		exportValue: {
+			amount: 2.0,
+			value: 2.0,
+		},
+	},
+	{
+		productId: 2,
+		productName: "Product 1",
+		productImageCode: "220622-853f8e3b-4f33-44d3-b0b4-ff1c98450498.jpg",
+		unit: "Unit 1",
+		startValue: {
+			amount: 11.0,
+			value: 10001.0,
+		},
+		endValue: {
+			realValue: {
+				amount: 11.0,
+				value: 10001.0,
+			},
+			calculateValue: {
+				amount: 11.0,
+				value: 10001.0,
+			},
+		},
+		importValue: {
+			amount: 15.0,
+			value: 10005.0,
+		},
+		exportValue: {
+			amount: 12.0,
+			value: 10002.0,
+		},
+	},
+	{
+		productId: 3,
+		productName: "Product 2",
+		productImageCode: "220622-853f8e3b-4f33-44d3-b0b4-ff1c98450498.jpg",
+		unit: "Unit 2",
+		startValue: {
+			amount: 21.0,
+			value: 20001.0,
+		},
+		endValue: {
+			realValue: {
+				amount: 21.0,
+				value: 20001.0,
+			},
+			calculateValue: {
+				amount: 21.0,
+				value: 20001.0,
+			},
+		},
+		importValue: {
+			amount: 25.0,
+			value: 20005.0,
+		},
+		exportValue: {
+			amount: 22.0,
+			value: 20002.0,
+		},
+	},
+	{
+		productId: 4,
+		productName: "Product 3",
+		productImageCode: "220622-853f8e3b-4f33-44d3-b0b4-ff1c98450498.jpg",
+		unit: "Unit 3",
+		startValue: {
+			amount: 31.0,
+			value: 30001.0,
+		},
+		endValue: {
+			realValue: {
+				amount: 31.0,
+				value: 30001.0,
+			},
+			calculateValue: {
+				amount: 31.0,
+				value: 30001.0,
+			},
+		},
+		importValue: {
+			amount: 35.0,
+			value: 30005.0,
+		},
+		exportValue: {
+			amount: 32.0,
+			value: 30002.0,
+		},
+	},
+];
